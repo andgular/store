4259875db623dd97b7b0462e46c3c3880fc99bd4
andgular <160800039+andgular@users.noreply.github.com>
2024-02-22 08:11:59 -0500

    step 2
    Pass data to a parent component.

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
-import { Product } from '../products';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Product } from "../products";
 @Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrl: './product-alerts.component.css',
+  selector: "app-product-alerts",
+  templateUrl: "./product-alerts.component.html",
+  styleUrl: "./product-alerts.component.css",
 })
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
